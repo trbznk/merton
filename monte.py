ff2aac7263cd8d993fcbc5b7962ac84f2d6a67da
trbznk <alexander.terbeznik@icloud.com>
2024-04-22 20:49:05 +0200

    init gui

--- monte.py
+++ monte.py
@@ -1,5 +1,9 @@
 import ctypes
 
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLineEdit, QLabel, QGridLayout, QGroupBox
+from PySide6.QtCharts import QChartView, QChart
+
 class Config(ctypes.Structure):
     _fields_ = [
         ("asset_corr", ctypes.c_float),
@@ -15,6 +19,56 @@ class Result(ctypes.Structure):
         ("L_PF", ctypes.POINTER(ctypes.c_float))
     ]
 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        main_layout = QGridLayout()
+        main_layout.setColumnStretch(1, 10)
+
+        input_groupbox = QGroupBox("Configuration")
+        self.asset_corr_input = QLineEdit()
+        self.cut_off_input = QLineEdit()
+        self.ead_input = QLineEdit()
+        self.lgd_input = QLineEdit()
+        self.pf_size_input = QLineEdit()
+        self.n_sims_input = QLineEdit()
+        self.simulate_button = QPushButton("Simulate")
+
+        input_layout = QGridLayout()
+        input_groupbox.setLayout(input_layout)
+        input_layout.addWidget(QLabel("Asset correlation:"), 0, 0, Qt.AlignRight)
+        input_layout.addWidget(self.asset_corr_input, 0, 1)
+        input_layout.addWidget(QLabel("Cut-Off-Value:"), 1, 0, Qt.AlignRight)
+        input_layout.addWidget(self.cut_off_input, 1, 1)
+        input_layout.addWidget(QLabel("EAD:"), 2, 0, Qt.AlignRight)
+        input_layout.addWidget(self.ead_input, 2, 1)
+        input_layout.addWidget(QLabel("LGD:"), 3, 0, Qt.AlignRight)
+        input_layout.addWidget(self.lgd_input, 3, 1)
+        input_layout.addWidget(QLabel("Portfolio size:"), 4, 0, Qt.AlignRight)
+        input_layout.addWidget(self.pf_size_input, 4, 1)
+        input_layout.addWidget(QLabel("Simulations:"), 5, 0, Qt.AlignRight)
+        input_layout.addWidget(self.n_sims_input, 5, 1)
+        input_layout.addWidget(self.simulate_button, 6, 1)
+
+        result_groupbox = QGroupBox("Result")
+        result_layout = QGridLayout()
+        result_groupbox.setLayout(result_layout)
+        result_layout.addWidget(QLabel("L_PF:"), 0, 0)
+
+        plotting_groupbox = QGroupBox("Plotting")
+        plotting_layout = QVBoxLayout()
+        plotting_groupbox.setLayout(plotting_layout)
+        chart_view = QChartView()
+        plotting_layout.addWidget(chart_view)
+
+        main_layout.addWidget(input_groupbox, 0, 0)
+        main_layout.addWidget(result_groupbox, 1, 0)
+        main_layout.addWidget(plotting_groupbox, 0, 1, 2, 1)
+
+        self.setWindowTitle("merton")
+        self.setLayout(main_layout)
+
 libmonte = ctypes.CDLL("./monte.so")
 libmonte.simulate.argtypes = [Config, Result]
 libmonte.simulate.restype = Result
@@ -23,6 +77,11 @@ result = Result((ctypes.c_float*1000)())
 config = Config(0.05, -2.0, 1.0, 0.6, 100, 1000)
 result = libmonte.simulate(config, result)
 
-print(result)
-for i in range(10):
-    print(result.L_PF[i])
+# print(result)
+# for i in range(10):
+#     print(result.L_PF[i])
+
+app = QApplication()
+window = Window()
+window.show()
+app.exec()
